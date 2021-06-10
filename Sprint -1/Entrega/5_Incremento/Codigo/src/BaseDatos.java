96f1b6e1d69af30bbb4d06d9dfce6aa54942a5f5
InakiArzalluz <inoooaak@gmail.com>
2021-06-10 20:25:46 -0300

    corrección de pasaje boolean en constructor de Secretaria

---
+++ Sprint -1/Entrega/5_Incremento/Codigo/src/BaseDatos.java
@@ -0,0 +1,27 @@
+public class BaseDatos {
+    //simulamos una base de datos donde vamos a trae los valores para comparar
+    private String nombreUsuario;
+    private String clave;
+
+    public BaseDatos() {
+        this.nombreUsuario = "Cecilia";
+        this.clave = "1234";
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String calve) {
+        this.clave = clave;
+    }
+
+}
